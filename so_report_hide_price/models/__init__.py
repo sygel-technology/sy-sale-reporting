d49e061e9e573ac2d8b1bd9b4fff92ae5110ed71
manu <manuel.regidor@sygel.es>
2022-07-13 14:09:55 +0200

    T-2911[ADD]so_report_hide_price, so_report_hide_price_portal

---
+++ so_report_hide_price/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import sale_order_line
+from . import sale_order
+from . import res_config_settings
---
+++ so_report_hide_price/models/sale_order_line.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    show_price_info_report = fields.Boolean(
+        string="Price in Report",
+        default=True
+    )
+
+    def _prepare_invoice_line(self, **optional_values):
+        values = super()._prepare_invoice_line(**optional_values)
+        if self.env['account.move.line']._fields.get('show_price_info_report'):
+            values['show_price_info_report'] = self.show_price_info_report
+        return values
