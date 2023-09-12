934cd1bbabda0df9dd0e8cc7ce72a07e4e3c9480
angel <angel.garcia@sygel.com>
2023-09-12 13:24:22 +0200

    [15.0][NEW] so_report_hide_lines: Show/Hide lines in sales PDF.

---
+++ so_report_hide_lines/models/sale_order_line.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    show_line_report = fields.Boolean(
+        string='Line in Report',
+        help="If True the line will be displayed in the sales "
+        "reports. If False, the line will not be displayed",
+        default=True
+    )
