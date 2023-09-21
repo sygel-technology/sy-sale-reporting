8f16a0f61eec25dd5f528d9fa312e0ecb46b7334
angel <angel.garcia@sygel.com>
2023-09-21 10:38:31 +0200

    [15.0][NEW] so_product_set_report_hide_lines: Show/Hide products from product set in sales PDF.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Available addons
 ----------------
 addon | version | summary
 --- | --- | ---
+[so_product_set_report_hide_lines](so_product_set_report_hide_lines/) | 15.0.1.0.0 | Show/Hide products from product set in sales PDF.
 [so_report_hide_lines](so_report_hide_lines/) | 15.0.1.0.0 | Show/Hide lines in sales PDF.
 [so_report_hide_price](so_report_hide_price/) | 15.0.1.0.0 | Show/Hide price info in sales PDF.
 [so_report_hide_price_portal](so_report_hide_price_portal/) | 15.0.1.0.0 | Show/Hide price info in sale portal view.
---
+++ so_product_set_report_hide_lines/wizard/product_set_add.py
@@ -0,0 +1,35 @@
+# Copyright 2023 Ángel García de la Chica Herrera <angel.garcia@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class ProductSetAdd(models.TransientModel):
+    _inherit = "product.set.add"
+
+    hide_set_lines = fields.Selection(
+        string="Show Lines in Sales Report",
+        selection=[
+            ('by_set_config', 'Depending on the set configuration'),
+            ('hide_lines', 'Hide All Lines'),
+            ('show_lines', 'Show All Lines')
+        ],
+        help="'Depending on the set configuration': All lines that have been "
+        "configured in the product set will be displayed in the sales report."
+        "\n'Hide all lines': all lines of the product set will be hidden in "
+        "the sales report.\n'Show all lines': all lines of the product set "
+        "will be displayed in the sales report.\nYou can also modify the "
+        "product set lines displayed in the sales report from the sales order.",
+        default='by_set_config',
+        required=True
+    )
+
+    def prepare_sale_order_line_data(self, set_line, max_sequence=0):
+        sol_data = super().prepare_sale_order_line_data(set_line, max_sequence)
+        if self.hide_set_lines == 'by_set_config' and\
+                not set_line.show_line_report:
+            sol_data.update({"show_line_report": False})
+        elif self.hide_set_lines == 'hide_lines' and\
+                not set_line.display_type:
+            sol_data.update({"show_line_report": False})
+        return sol_data
