2d590caf51f6cd59312bb039426c3f004c901698
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 11:10:15 +0200

    [T-5944][ADD] First commit

--- so_report_hide_price/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import sale_order_line
-from . import sale_order
-from . import res_config_settings
