9421e44e3fbb844f5536456066bfada35700f8d1
sygel <alberto.martinez@sygel.es>
2026-05-14 11:43:27 +0200

    [T-9594][IMP] so_report_hide_salesperson: pre-commit auto fixes

--- so_report_hide_salesperson/__manifest__.py
+++ so_report_hide_salesperson/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "15.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/sygel-technology/sy-sale-reporting",
-    "author": "Sygel, Odoo Community Association (OCA)",
+    "author": "Sygel",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
